23c0ea9d1d5b572bb2cb495df178db46440a8170
outbounder <obiwonn@gmail.com>
2018-09-06 18:30:05 +0300

    update cron cell to implement abstract organic-worker-interval

--- seed/cells/{{{cell-name}}}/organelles/{{{cell-name}}}.js
+++ seed/cells/{{{cell-name}}}/organelles/{{{cell-name}}}.js
@@ -1,43 +1,7 @@
-module.exports = class {
-  constructor (plasma, dna) {
-    this.plasma = plasma
-    this.dna = dna
-    this.running = false
-    this.executeLock = null
-    this.intervalID = null
-    this.exceptionThrown = null
-    this.executeCallSkipped = false
+const IntervalWorker = require('organic-worker-interval')
 
-    plasma.on(dna.disposeOn || 'kill', this.dispose, this)
-    plasma.on(dna.startOn || '{{{cell-name}}}JobStart', this.start, this)
-    if (dna.startOnInitialization) {
-      this.start()
-    }
-  }
-  start () {
-    if (this.running) return console.warn('already started')
-    this.running = true
-    this.intervalID = setInterval(() => {
-      if (this.executeLock) {
-        this.executeCallSkipped = true
-        return console.warn('skipping execute call, still in execution cycle!')
-      }
-      this.executeLock = this.execute()
-      this.executeLock.then(() => {
-        this.executeLock = null
-        this.executeCallSkipped = false
-      })
-    }, this.dna.intervalMiliseconds)
-  }
+module.exports = class extends IntervalWorker {
   async execute () {
-    console.log('running') // TODO add your cron execute logic
-  }
-  dispose () {
-    console.log('terminating')
-    if (this.intervalID) {
-      clearInterval(this.intervalID)
-    }
-    this.intervalID = null
-    this.running = false
+    console.log('running')
   }
 }
--- seed/cells/{{{cell-name}}}/organelles/{{{cell-name}}}.test.js
+++ seed/cells/{{{cell-name}}}/organelles/{{{cell-name}}}.test.js
@@ -3,16 +3,8 @@ const Plasma = require('organic-plasma')
 
 test('executes', async () => {
   let plasma = new Plasma()
-  let dna = {}
+  let dna = {log: true}
   let instance = new OrganelleJob(plasma, dna)
   await instance.execute()
-  instance.dispose()
-})
-
-test('starts and disposes', async () => {
-  let plasma = new Plasma()
-  let dna = {}
-  let instance = new OrganelleJob(plasma, dna)
-  instance.start()
   instance.dispose()
 })
